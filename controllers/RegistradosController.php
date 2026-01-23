28cc145e479b7c88ca6d97afa856b7a99057ab0b
Maty1337 <matidavidbuena@gmail.com>
2026-01-23 13:02:50 -0300

    feat(AdminPanelv1): Finalizacion del Panel de Administracion

--- controllers/RegistradosController.php
+++ controllers/RegistradosController.php
@@ -3,13 +3,50 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Paquete;
+use Model\Ponente;
+use Model\Usuario;
+use Model\Registro;
+use Classes\Paginacion;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class RegistradosController {
 
     public static function index(Router $router) {
+
+    
+        if(!isAdmin()){
+            header('Location: /login');        
+            }
+
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+        if(!$pagina_actual || $pagina_actual < 1){
+            header('Location: /admin/registrados?page=1');
+        }
+
+        $registros_por_pagina = 10;
+        $total = Registro::total();
+        $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
+        if( $paginacion->total_paginas() < $pagina_actual ) {
+            header('Location: /admin/registrados?page=' . $paginacion->total_paginas() );
+        }
+
+        $registros = Registro::paginar($registros_por_pagina, $paginacion->offset());
+        foreach($registros as $registro){
+            $registro->usuario = Usuario::find($registro->usuario_id);
+            $registro->paquete = Paquete::find($registro->paquete_id);
+        }
+
+
+
         // Render a la vista 
         $router->render('admin/registrados/index', [
-            'titulo' => 'Registrados / Conferencistas'
+            'titulo' => 'Registrados / Conferencistas',
+            'registros' => $registros,
+            'paginacion' => $paginacion->paginacion()
         ]);
     }
 }
